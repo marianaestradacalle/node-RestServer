57e106ef2835f1d63ceb117e2c033bd7bb505ca6
marianaestradacalle <estradacallemariana@gmail.com>
2020-05-18 12:15:50 -0500

    Variable de ENTORNO MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://Mariec:tkG5IacnM9DoHQpM@cluster0-tfvfd.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
